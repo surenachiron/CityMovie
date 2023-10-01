4c2422df47b68fb0e03342d6dc15ac632a08dd9f
mohammad karimi <mohammadkarimi.swctw@gmail.com>
2023-10-01 15:23:12 +0330

    Update Trending-Movie.jsx

--- component/pages/movies/Trending-Movie.jsx
+++ component/pages/movies/Trending-Movie.jsx
@@ -39,31 +39,6 @@ const TrendingMovie = ({ movies }) => {
                             </Link>
                         </Box>
                     )) : ""}
-                    {movies.message === undefined ? movies.map((movie) => (
-                        <Box key={movie.id.slice(7, -1)} sx={{ width: "140px", minWidth: "140px", m: "10px", p: "0", display: "inline-block" }}>
-                            <Link href={`/movies/${movie.id.slice(7, -1)}`} style={{ width: "100%" }}>
-                                <Box sx={{
-                                    display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", position: "relative", width: "100%", height: "250px"
-                                }}>
-                                    {movie.image.url ? (
-                                        <>
-                                            <Image
-                                                src={movie.image.url}
-                                                alt={movie.title}
-                                                width={140}
-                                                height={250}
-                                                priority={true}
-                                            />
-                                            <HoverImage />
-                                        </>)
-                                        : (<Skeleton variant="rectangular" width={210} height={118} animation="wave" />)}
-                                </Box>
-                                <Box>
-                                    <Typography variant="subtitle1"><bdi >{movie.title.length >= 13 ? movie.title.slice(0, 13) + "..." : movie.title}</bdi></Typography>
-                                </Box>
-                            </Link>
-                        </Box>
-                    )) : ""}
                 </Box>
             </Box >
         </>
