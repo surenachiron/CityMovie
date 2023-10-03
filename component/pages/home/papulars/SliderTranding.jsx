ddd71e67858fac3cb3ff9d048741a9891aa6627a
Mohammad Karimi <mohammadkarimi.swctw@gmail.com>
2023-10-03 23:20:56 +0330

    fix bog

--- component/pages/home/papulars/SliderTranding.jsx
+++ component/pages/home/papulars/SliderTranding.jsx
@@ -11,28 +11,32 @@ import SwiperSliderTranding from './SwiperSliderTranding';
 const SliderTranding = ({ commingSoonMovie }) => {
 
     return (
-        <Box sx={{ display: "flex", justifyContent: "center" }}>
-            <Box sx={{ width: { md: "65%", xs: "100%" }, height: { md: "70vh", xs: "80vh" } }}>
-                <Swiper
-                    spaceBetween={30}
-                    centeredSlides={true}
-                    autoplay={{
-                        delay: 4000,
-                        disableOnInteraction: false
-                    }}
-                    navigation={true}
-                    modules={[Autoplay, Pagination, Navigation]}
-                    className="mySwiper"
-                >
-                    {commingSoonMovie.map(movie => (
-                        <SwiperSlide className='swiper-slide' key={movie.id}>
-                            <SwiperSliderTranding image={movie.title.image.url} title={movie.title.title} relaseData={movie.releaseDate} ratings={movie.ratings} genres={movie.genres} id={movie.id} />
-                        </SwiperSlide>
-                    ))
-                    }
-                </Swiper>
-            </Box>
-        </Box>
+        <>
+            {commingSoonMovie.length > 1 ?
+                <Box sx={{ display: "flex", justifyContent: "center" }}>
+                    <Box sx={{ width: { md: "65%", xs: "100%" }, height: { md: "70vh", xs: "80vh" } }}>
+                        <Swiper
+                            spaceBetween={30}
+                            centeredSlides={true}
+                            autoplay={{
+                                delay: 4000,
+                                disableOnInteraction: false
+                            }}
+                            navigation={true}
+                            modules={[Autoplay, Pagination, Navigation]}
+                            className="mySwiper"
+                        >
+                            {commingSoonMovie.map(movie => (
+                                <SwiperSlide className='swiper-slide' key={movie.id}>
+                                    <SwiperSliderTranding image={movie.title.image.url} title={movie.title.title} relaseData={movie.releaseDate} ratings={movie.ratings} genres={movie.genres} id={movie.id} />
+                                </SwiperSlide>
+                            ))
+                            }
+                        </Swiper>
+                    </Box>
+                </Box >
+                : ""}
+        </>
     );
 }
 
