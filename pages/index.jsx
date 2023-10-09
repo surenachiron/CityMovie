feb8e5ed9e4f300c61cc21f4d868936562fe84aa
mohammad karimi <mohammadkarimi.swctw@gmail.com>
2023-10-09 17:41:08 +0330

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -31,9 +31,9 @@ export async function getServerSideProps() {
 
   /// we use this approache with setTimeout because we have limit in request to api actually we can 10 request per second 
   const [datacommingsoon, datatrendMovies, datatrendTvShows] = await Promise.all([
-    await new Promise(resolve => setTimeout(() => resolve(getCommingSoonMovie()), 1500)),
-    await new Promise(resolve => setTimeout(() => resolve(getPapularMovies()), 1500)),
-    await new Promise(resolve => setTimeout(() => resolve(getPapularTvShows()), 1500)),
+    await new Promise(resolve => setTimeout(() => resolve(getCommingSoonMovie()), 1000)),
+    await new Promise(resolve => setTimeout(() => resolve(getPapularMovies()), 1000)),
+    await new Promise(resolve => setTimeout(() => resolve(getPapularTvShows()), 1000)),
   ]);
   const imagesGenres = getGenresList()
 
