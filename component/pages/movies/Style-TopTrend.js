41ecfc0b48e8574b67d324a9495000f15a0bc892
Mohammad Karimi <mohammadkarimi.swctw@gmail.com>
2023-10-15 13:24:25 +0330

    fix bog

--- component/pages/movies/Style-TopTrend.js
+++
@@ -1,6 +0,0 @@
-const classes = {
-    boxParent: { mt: "1rem" },
-    titleTrendTop: { mb: "7px" }
-}
-
-export default classes
--- pages/500.js
+++
@@ -1,9 +0,0 @@
-const ServerErrorPage = () => {
-    return (
-        <div>
-            Error
-        </div>
-    );
-}
-
-export default ServerErrorPage;
